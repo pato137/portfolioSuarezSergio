bc5414b456b41340f21f5d38449cb91aedc8c4b2
Suarez Sergio <66366215+pato137@users.noreply.github.com>
2020-11-18 09:08:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-btn').addEventListener('click',( ) => {
+   document.querySelector('.nav-menu').classList.toggle('show');
+    
+});
+
+document.querySelector('.cards').addEventListener('mouseover',()=>{
+//alert("helloworld");
+
+
+
+})
+
